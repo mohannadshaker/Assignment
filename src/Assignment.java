959640fb3ccbd06544b461f85f2278f493bed627
علاء محمد <alaamohameed328@gmail.com>
2025-12-06 22:43:06 +0300

    v2

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,5 +1,5 @@
 public class Assignment {
     public static void main(String[] args) {
-        System.out.println("Assignment done...");
+        System.out.println("Assignment done..");
     }
 }
